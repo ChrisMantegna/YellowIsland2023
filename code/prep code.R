3e3fcac78b7407467638966c94ece6be19d1dfc1
Chris Mantegna <christine.mantegna@gmail.com>
2023-06-18 15:34:21 -0700

    updating structure

---
+++ code/prep code.R
@@ -0,0 +1,6 @@
+install.packages('rsconnect')
+rsconnect::setAccountInfo(name='cmantegna',
+                          token='B31FB859EE0EA24F3CA09978F715745D',
+                          secret='i0wHn5QMkJ7dyd1bbA7nCI4zxvqG8CJY90+U3yXW')
+library(knitr, "quarto")
+install.packages("quarto")
